b8a657959822680b43d39d799efe23d566cae018
Gautham Stalin <Gautham.Stalin@monitise.com>
2014-11-12 16:56:27 -0800

    web intent

---
+++ gs-angular-directives.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('gsDirectives', [])
+    .directive('webIntent', [
+        '$timeout',
+        function ($timeout) {
+            return {
+                templateUrl: '/views/directives/web-intent.html',
+                restrict: 'E',
+                replace: true,
+                controller: function postLink($scope) {
+                    var timer;
+                    $scope.$on('$alert', function (event, message) {
+                        var display = 10 * 1000;
+                        if (message.showTime) {
+                            display = message.showTime;
+                        }
+                        $scope.message = message.message;
+                        $timeout.cancel(timer);
+                        timer = $timeout(function () {
+                            $scope.message = null;
+                        }, display);
+                    });
+                }
+            };
+        }
+    ])
